5840002eb6858a75d1576be9da22edd29da78920
Hesham-XQ <hesham001230@gmail.com>
2021-10-06 15:35:28 +0200

    adding new things

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=heshamxq/uda_ml
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag uda_ml $dockerpath
+docker login --username=heshamxq
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
